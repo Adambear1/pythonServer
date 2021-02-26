78fe25bd1829abd9ec02ce065d1ee9e1d8ce9c06
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-02-25 19:14:37 -0700

    finished updating format_data file with simple tsting

--- format_data.py
+++ format_data.py
@@ -4,12 +4,39 @@ import json
 class GatherData():
     def __init__(self):
         self.file = lambda x: open("storage.json", x)
+
         self.data = json.loads(self.file("r").read())
 
-    def getAll(self):
-        return self.data
+    @property
+    def updateFile(self):
+        return self.file("w").write(json.dumps(self.data))
+
+    def get(self, index=None):
+        if index is None:
+            return self.data
+        else:
+            return self.data[index]
 
     def add(self, item):
-        self.data.append(item)
-        self.file("w").write(self.data)
-        print(self.data)
+        if type(item) is list:
+            self.data = [*item, *self.data]
+            self.updateFile
+            return self.data
+        else:
+            self.data.append(item)
+            self.updateFile
+            return self.data
+
+    def remove(self, item):
+        self.data.remove(item)
+        self.updateFile
+        return self.data
+
+
+# g = GatherData().get(3)
+
+# a = GatherData().add("Welcome!!")
+
+r = GatherData().remove("a")
+
+print(r)
